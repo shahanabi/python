825e1eb7821230a28901fb94b7c830bbbfc38a64
shahanabi <sahanabishabi145@gmail.com>
2024-09-05 20:08:20 +0300

    Add files via upload

---
+++ python datatypes.py
@@ -0,0 +1,14 @@
+fruits = ['apple','banana', 'cherry']
+second_element=fruits[1]
+print(second_element)
+
+fruits.append("orenge")
+fruits.remove("banana")
+print(fruits)
+
+numbers=['1','2','3','4','5','6','7','8','9','10','11']
+first_five=numbers[:5]
+print(first_five)
+
+squares=[x**2 for x in range(1,11)]
+print(squares)
