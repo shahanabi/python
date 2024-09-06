159e511529db624aec8211b28065235f8d445d42
shahanabi <sahanabishabi145@gmail.com>
2024-09-06 18:57:37 +0300

    Add files via upload

---
+++ taskprime.py
@@ -0,0 +1,20 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    
+    return True
+
+numbers = [ 3, 4, 25, 29]
+
+for number in numbers:
+    print(f"{number} is prime: {is_prime(number)}")
